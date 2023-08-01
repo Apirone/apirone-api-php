ee2a7a9fa24a1758d60ba06148238b5503e56e2d
alex <alex.zaytseff@gmail.com>
2023-08-01 15:06:36 +0400

    Update file headers

--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the Apirone SDK library.
+ * This file is part of the Apirone API library.
  *
  * (c) Alex Zaytseff <alex.zaytseff@gmail.com>
  *
--- src/Log/LoggerWrapper.php
+++ src/Log/LoggerWrapper.php
@@ -1,12 +1,14 @@
 <?php
 /*
- * This file is part of the Apirone SDK library.
+ * This file is part of the Apirone API library.
  *
  * (c) Alex Zaytseff <alex.zaytseff@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Apirone\API\Log;
 
