6eaf77590dcedfc61b060ca6b59c193f2592bff3
alex <alex.zaytseff@gmail.com>
2023-08-16 20:02:37 +0400

    Docker structure refactoring

---
+++ examples/common.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+use Apirone\API\Log\LoggerWrapper;
+
+function pa($mixed, $title = false)
+{
+    if ($title) {
+        echo $title . ':';
+    }
+    echo '<pre>';
+    if (gettype($mixed) == 'boolean') {
+        print_r($mixed ? 'true' : 'false');
+    }
+    else {
+        print_r($mixed !== null ? $mixed : 'NULL');
+    }
+    echo '</pre>';
+}
+
+
+$loggerCallback = static function($level, $message, $context) {
+    $log_file ='/var/www/html/log.txt';
+    $data = [$level, $message, $context];
+    file_put_contents($log_file, print_r($data, true) . "\r\n", FILE_APPEND);
+};
+
+LoggerWrapper::setLogger($loggerCallback);
---
+++ examples/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once ('common.php');
+
+use Apirone\API\Endpoints\Service;
+
+$info = Service::account();
